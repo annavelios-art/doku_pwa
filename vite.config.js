19106f14131d2343a54d8b102454511d9c908fb4
Anna <anna.velios@googlemail.com>
2026-06-10 07:43:24 +0200

    vite anpassung

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,26 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Behandlungsdokumentation',
+        short_name: 'Doku',
+        display: 'standalone',
+        theme_color: '#f5f6f8',
+        background_color: '#f5f6f8',
+        icons: [
+          {
+            src: '/logo_kl.gif',
+            sizes: '192x192',
+            type: 'image/gif'
+          }
+        ]
+      }
+    })
+  ]
 })
